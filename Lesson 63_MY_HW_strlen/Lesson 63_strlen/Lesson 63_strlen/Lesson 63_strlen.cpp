fb10e48d0b0860d50b81b54543c3f165e0359860
Ivan Schucin <ivanschucin@yandex.ru>
2024-08-12 16:58:54 +0300

    Lesson 115 Edit Files

---
+++ Lesson 63_MY_HW_strlen/Lesson 63_strlen/Lesson 63_strlen/Lesson 63_strlen.cpp
@@ -0,0 +1,44 @@
+﻿// Lesson 63_strlen.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int  strLen(const char* Char);
+
+
+
+
+
+int main()
+{
+
+    const char* strArr[] = { "Hello","World","!",};
+
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << strArr[i];
+    }
+    cout << '\n';
+
+    const char *stri = "Hall";
+    //cout << stri << '\n';
+    //cout << strlen(stri);
+    cout << stri << '\n'; 
+     cout << strLen(stri);
+
+}
+
+
+int  strLen(const char* Char)
+{
+    int counter = 0;
+
+    while (Char[counter] != '\0')
+    {
+        counter++;
+    }
+ 
+
+    return counter;
+}
---
+++ Lesson 92 ID_generator/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,52 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Humen
+{
+public:
+	Humen(string const _Firtst_Name, string const _Second_Name, int _age):Firtst_Name(_Firtst_Name),Second_Name(_Second_Name),age(_age)
+	{
+		ID = count++;
+		
+	}
+	void GetInfo()
+	{
+		cout << ID << " " << Firtst_Name << " " << Second_Name << " " << age << "\n";
+	}
+	static int GetCount() {
+		
+		return count;
+	}
+	~Humen() {};
+
+private:
+	string Firtst_Name;
+	string Second_Name;
+	int age;
+	int ID;
+	static int count;
+	
+};
+
+
+
+int Humen::count = 0;
+
+
+
+
+int main()
+{
+	Humen FirstHumen("Ioan", "Shucin", 10);
+	FirstHumen.GetInfo();
+	Humen SecondHumen("Ioan2", "Shucin2", 15);
+	SecondHumen.GetInfo();
+	Humen ThirdHumen("Ioan3", "Shucin3", 20);
+	ThirdHumen.GetInfo();
+	 
+	cout<<'\n'<<Humen::GetCount();
+}
