b6e2d0bab56624c9de966cba55b6f9552db6ce2b
Ivan Schucin <ivanschucin@yandex.ru>
2024-09-03 11:46:57 +0300

    add Lesson_123_124

---
+++ Lesson 123 exception/Lesson 122 exception/Lesson 122 exception.cpp
@@ -0,0 +1,99 @@
+﻿
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+//class Point
+//{
+//public:
+//	Point() { x = y = z = 0; }
+//	Point(int x, int y, int z) { this->x = x; this->y = y; this->z = z; }
+//
+//private:
+//	int x;
+//	int y;
+//	int z;
+//	friend ostream& operator<<(ostream& os, const Point& point);
+//	friend istream& operator>> (istream& is, Point& point);
+//};
+// 
+//ostream& operator<<(ostream& os, const Point& point)
+//{
+//	os << point.x << " " << point.y << " " << point.z;
+//	return os;
+//}
+// 
+//istream& operator>> (istream& is, Point& point)
+//{
+//	is >> point.x >> point.y >> point.z;
+//	return is;
+//}
+
+
+
+class Myexception : public exception
+{
+public:
+	Myexception (const char *msg, int data) :exception(msg)
+	{
+		this->dataState = data;
+	}
+
+	int GetDataState() { return dataState; }
+
+private:
+	int dataState;
+};
+
+
+
+
+
+
+void Foo(int value)
+{
+	
+	if (value < 0)
+	{
+
+		throw exception("Number less null");
+		
+	}
+	if (value == 1)
+	{
+		throw Myexception("Number = 1 !!!", value);
+	}
+
+	cout << "Value= " << value << endl;
+}
+
+
+
+ 
+
+
+int main()
+{
+	try
+	{ 
+		Foo(6258935);
+	}
+	catch (Myexception& a)
+	{
+
+		cout << "1. We have caught a number - " << a.what() << endl;
+		cout << "Data status - " << a.GetDataState() << endl;
+	}
+	catch(exception& a)
+	{
+		cout << "2. We have caught a number - " << a.what() << endl;
+	}
+		
+		
+	
+
+	return 0;
+}
---
+++ Lesson 124 ENUM/Lesson 124 ENUM/Lesson 124 ENUM.cpp
@@ -0,0 +1,69 @@
+﻿// Lesson 124 ENUM.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+class PC
+{
+public:
+	enum PCState
+	{
+		OFF,
+		On,
+		Sleep
+	};
+	PCState GetState() { return State; }
+
+	void SetState(PCState State)
+	{
+		this->State = State;
+	}
+private:
+	PCState State;
+};
+
+
+enum Speed {
+	MIN=150,
+	RECOMEND=600,
+	MAX=800
+};
+
+int main()
+{
+
+	PC pc;
+	pc.SetState(PC::PCState::On);
+
+
+		//if (pc.GetState()==PCState::On)
+		//{
+		//	cout << "PC ON" << endl;
+		//}
+
+
+	Speed sp = Speed::MAX;
+	cout << sp << endl;
+
+
+
+
+	switch (pc.GetState())
+	{
+
+	case PC::PCState::OFF:
+		cout << "PC OFF" << endl;
+			break;
+
+	case PC::PCState::On:
+		cout << "PC ON" << endl;
+
+		break;
+	case PC::PCState::Sleep:
+		cout << "PC Sleep" << endl;
+		break;
+	}
+}
