d8d2d05124bee8982cb7a50640e29ffef4dcecb7
Ivan Schucin <ivanschucin@yandex.ru>
2024-09-24 17:00:48 +0300

    Lesson STL 5 forward_list

---
+++ Lesson 137 foreach/Lesson 137 foreach/Lesson 137 foreach.cpp
@@ -0,0 +1,41 @@
+﻿// Lesson 137 foreach.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+template<typename T>
+void PrintList(const list<T>& lst) {
+
+	for (const auto& i : lst)
+	{
+		cout << i << '\n';
+	}
+
+
+	/*for (auto i = lst.cbegin(); i != lst.cend(); i++)
+	{
+
+		cout << *i << '\n';
+	}*/
+	cout << '\n';
+}
+
+int main()
+{
+	//int arr[]={ 5,11,94,99,44 };
+	list<int> a={ 5,11,94,99,44 };
+
+	PrintList(a);
+
+	for(auto &var: a)
+	{		
+		var = 1;
+	}
+
+	PrintList(a);
+	
+}
---
+++ Lesson STL 5 forward list/Lesson STL 5 forward list/Lesson STL 5 forward list.cpp
@@ -0,0 +1,36 @@
+﻿// Lesson STL 5 forward list.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <forward_list>
+
+
+
+int main()
+{
+    std::forward_list<int>f1{ 131,94,494 };
+    
+   
+
+    std::forward_list<int>::iterator it = f1.begin();
+    /*++it;*/
+    /* --it*/ //не работает
+    
+    //f1.insert_after(it, 9999);
+    f1.erase_after(it);
+
+    f1.before_begin();
+    f1.insert_after(it, 9999);
+       
+
+    //std::cout << *it << std::endl;
+    
+    
+       for (auto i : f1)
+    {
+        std::cout << i << std::endl;
+    }
+    std::cout << std::endl;
+
+}
+
